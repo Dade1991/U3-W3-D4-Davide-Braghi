8121bca10e7140dc33f846824d685e04366a785c
Dade1991 <dade91@msn.com>
2025-08-07 15:45:45 +0200

    Minor changes

--- (Boilerplate, giorno precedente) d9-hw-end/src/components/CompanySearchResults.jsx
+++ (Boilerplate, giorno precedente) d9-hw-end/src/components/CompanySearchResults.jsx
@@ -1,19 +1,19 @@
-import { useEffect, useState } from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
-import Job from './Job'
-import { useParams } from 'react-router-dom'
+import { useEffect, useState } from "react"
+import { Container, Row, Col } from "react-bootstrap"
+import Job from "./Job"
+import { useParams } from "react-router-dom"
 
 const CompanySearchResults = () => {
   const [jobs, setJobs] = useState([])
   const params = useParams()
 
-  const baseEndpoint =
-    'https://strive-benchmark.herokuapp.com/api/jobs?company='
-
   useEffect(() => {
     getJobs()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
+  const baseEndpoint =
+    "https://strive-benchmark.herokuapp.com/api/jobs?company="
 
   const getJobs = async () => {
     try {
@@ -22,7 +22,7 @@ const CompanySearchResults = () => {
         const { data } = await response.json()
         setJobs(data)
       } else {
-        alert('Error fetching results')
+        alert("Error fetching results")
       }
     } catch (error) {
       console.log(error)
